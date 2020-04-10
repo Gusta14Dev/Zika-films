a050503a64f31365cb286d4ad984d6c4b751cf2e
Gustavo Guimarães Cervo <gustavo.cervo14@gmail.com>
2020-04-10 18:23:36 -0300

    1° commit

---
+++ includes/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = 'usbw';
+$banco = 'filmeseseries';
+
+$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+
+if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+?>
